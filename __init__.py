0583aef3ce511e58fce7ec9075fc3511ceb24ca5
monajm36 <mjm36@uchicago.edu>
2025-08-05 10:37:22 -0500

    Add package initialization file

---
+++ __init__.py
@@ -0,0 +1,13 @@
+"""
+OHCA Classifier Package
+
+A BERT-based classifier for Out-of-Hospital Cardiac Arrest (OHCA) detection in medical text.
+"""
+
+from .ohca_classifier import OHCAClassifier
+
+__version__ = "1.0.0"
+__author__ = "monajm36"
+__email__ = "mjm36@uchicago.edu"
+
+__all__ = ["OHCAClassifier"]
